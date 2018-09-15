2087bd9803b9b2cccc83ebc0da39cb9d3d076641
Rodrigo Martins <drigo.oliveira94@gmail.com>
2018-09-15 12:00:35 -0300

    modificando a grid

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,7 +3,7 @@
 
     <div class="row">
     @foreach($results as $result)
-    @if ($loop->index < 8)
+    @if ($loop->index < 9)
         <div class="col-xs-6 col-md-4" style="margin-bottom: 15px">
             <a href="{!! $result->link !!}" class="thumbnail">
                 <img src="{{ $result->images->standard_resolution->url }}" alt="..." class="img-thumbnail">
